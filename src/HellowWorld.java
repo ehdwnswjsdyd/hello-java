87b48d108c84e9e7d8e48076eb6e4b6f078b55fb
ehdwnswjsdyd <ehdwnswjsdyd@naver.com>
2021-12-28 22:42:13 +0900

    구구단

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -3,6 +3,16 @@ public class HellowWorld {
 	public static void main(String[] args) {
 	System.out.println("Hellow_Java");
 	System.out.println("git update");
-	}
+	int a,b = 0;
 	
+	for (a = 2 ; a < 6 ; a++) {
+		{
+		for (b= 1 ; b <10; b++)
+			System.out.println(a*b);
+		}
+		System.out.println(a+"´Ü Á¾·á");
+	
+	
+	}
+	}
 }

35e68571672636132235745decdf502904964624
ehdwnswjsdyd <ehdwnswjsdyd@naver.com>
2021-12-31 22:03:25 +0900

    7,8단 이중 반복문 for, while 이용

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -1,18 +1,19 @@
 
 public class HellowWorld {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 	System.out.println("Hellow_Java");
 	System.out.println("git update");
-	int a,b = 0;
+	int a = 1 ,b = 0;
 	
-	for (a = 2 ; a < 6 ; a++) {
+	while (a < 9) 
 		{
-		for (b= 1 ; b <10; b++)
-			System.out.println(a*b);
+			a++;
+			{
+				for (b= 1 ; b <10; b++)
+					System.out.println(a*b);
+			}
+			System.out.println(a+"단 종료");
 		}
-		System.out.println(a+"단 종료");
-	
-	
-	}
 	}
 }

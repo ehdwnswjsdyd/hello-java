78104b6b94873df524ba0f2144d3fd095aef43f6
ehdwnswjsdyd <ehdwnswjsdyd@naver.com>
2022-01-01 13:19:34 +0900

    급한대로 배열 사용해봄

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -1,19 +1,29 @@
+import java.util.Scanner;
 
 public class HellowWorld {
 	public static void main(String[] args) 
 	{
-	System.out.println("Hellow_Java");
-	System.out.println("git update");
-	int a = 1 ,b = 0;
+	int[] result = new int[9];
+	System.out.println("구구단 중 출력할 단은? : ");
+	Scanner scanner = new Scanner(System.in);
+	int number = scanner.nextInt();
+	System.out.println("number : " + number);
+
+	if (number < 2 ) {
+		System.out.println("2단 이상의 단을 입력해주세요 ");
+	}
+	else if (number > 9) {
+		System.out.println("9단 이하의 단을 입력해주세요");
+	}
+	else {
+		
 	
-	while (a < 9) 
+		for (int i= 1 ; i <10; i++) 
 		{
-			a++;
-			{
-				for (b= 1 ; b <10; b++)
-					System.out.println(a*b);
-			}
-			System.out.println(a+"단 종료");
+			result[i-1] = i*number;
+			System.out.println(number+"*"+i+" 는 "+result[i-1]+" 입니다");
+			
 		}
+	}
 	}
 }

16ae0fad34b7e3cc470e7b005382cc7c43d58d6c
LDJ <LDJ@DESKTOP-A0PCJ3E>
2022-01-03 14:03:39 +0900

    자바책 하는중

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
-
+import java.util.Scanner;/*ctrl+shift+O 이클립스 자바 import*/
 public class HellowWorld {
 	public static void main(String[] args) 
 	{
 	int[] result = new int[9];
+	String yp = "입니다";
 	System.out.println("구구단 중 출력할 단은? : ");
+
 	Scanner scanner = new Scanner(System.in);
+
 	int number = scanner.nextInt();
 	System.out.println("number : " + number);
 
@@ -22,6 +24,7 @@ public class HellowWorld {
 		{
 			result[i-1] = i*number;
 			System.out.println(number+"*"+i+" 는 "+result[i-1]+" 입니다");
+			System.out.printf("%d*%d 는 %d %s -형식지정자-\r", number, i,result[i-1], yp /*형식지정자 이걸로 쓰면 예쁘네*/);
 			
 		}
 	}
